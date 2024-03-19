79e4fb6c36fbb4c3b6ded028777bc3167d4d71c3
JacopoLombardi <lombajacopo@gmail.com>
2024-03-19 14:46:54 +0100

    [add] Compilated Readme File and added comment into code

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# vue-email-list
+vue email list
+===
+1. Inizio a creare il setup di Vue.
+2. Creo la chiamata all'API.
+3. Gestisco la sua risposta.
+4. Ripeto la chiamata per 10 volte quando clicco il pulsante tramite una funzione.
--- js/script.js
+++ js/script.js
@@ -10,7 +10,7 @@ createApp({
 
          resultArray: [],
          apiUrl: 'https://flynn.boolean.careers/exercises/api/random/mail',
-         randomEmail: '',
+         randomEmail: ''
 
       };
    },
@@ -19,14 +19,14 @@ createApp({
 
    
    methods:{
-
+      // ciclo la funzione della chiamata per 10 volte
       forLoop(){
          for(let i = 0; i<10; i++){
-            this.getApi()
+            this.getApi();
          }
       },
 
-
+      // chiamo la API e ricevo una risposta che utilizzo a mio piacere
       getApi(){
          axios.get(this.apiUrl)
    
